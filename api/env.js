cb7ab8dcf0d96cd99f5678692b6cb2dbebc63657
Cameo <cameo@42lyon.fr>
2025-11-28 18:39:31 +0100

    feat(auth): block authentication from users outside of a specified campus

---
+++ api/env.js
@@ -0,0 +1,3 @@
+module.exports = env = {
+	CAMPUS_ID: Number(process.env.CAMPUS_ID)
+}
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,12 +2,13 @@ const express = require("express");
 const api42 = require("../api42");
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const Users = require("../models/Users");
+const env = require("../env");
 const router = new express.Router();
 
 router.get('/42', (req, res) => {
 	res.redirect(api42.getOAuthUrl());
 });
-  
+
 router.get('/42/callback', async (req, res) => {
 	const code = req.query.code;
 	try {
@@ -15,6 +16,10 @@ router.get('/42/callback', async (req, res) => {
 		const token = await api42.generateUserToken(code);
 
 		const me = await api42.whoAmI(token);
+
+		if (!isFromCampus(me)) {
+			return res.redirect(`${process.env.FRONTEND_URL}?error=external_campus`);
+		}
 
 		const groups = await api42.fetch(`/v2/users/${me.id}/groups`);
 
@@ -45,18 +50,22 @@ router.get('/42/callback', async (req, res) => {
 		return res.status(500).send("Internal server error");
 	}
 
-	return res.status(200).redirect(process.env.FRONTEND_URL);
-
+	return res.redirect(process.env.FRONTEND_URL);
 });
 
 router.get('/logout', isLoggedIn, async (req, res) => {
 	try {
-        await req.session.destroy();
-    } catch (e) {
-        console.error(e);
-        return res.status(500).send('Error logging out');
-    }
-	return res.status(200).redirect(process.env.FRONTEND_URL);
+		await req.session.destroy();
+	} catch (e) {
+		console.error(e);
+		return res.status(500).send('Error logging out');
+	}
+	return res.redirect(process.env.FRONTEND_URL);
 });
 
 module.exports = router;
+
+function isFromCampus(user) {
+	const primaryCampus = user.campus_users.find(campusUser => campusUser.is_primary);
+	return primaryCampus?.campus_id === env.CAMPUS_ID;
+}
